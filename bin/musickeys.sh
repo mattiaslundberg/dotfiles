13eccaa2bca6894bec18527cdc56fc1198b65a60
Mattias Lundberg <mattiaslundberg@openmailbox.org>
2015-11-15 18:54:29 +0100

    Fix mute/unmute.

--- bin/musickeys.sh
+++ bin/musickeys.sh
@@ -10,8 +10,17 @@ case $1 in
    "prev")
        dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous
        ;;
+    "mute")
+       if amixer -c 0 get Master | grep -q off ; then
+           amixer set Master unmute
+           amixer set Headphone unmute
+           amixer set Speaker unmute
+       else
+           amixer set Master mute
+       fi
+   ;;
    *)
-       echo "Usage: $0 play|next|prev"
+       echo "Usage: $0 play|next|prev|mute"
        exit 1
         ;;
 esac
