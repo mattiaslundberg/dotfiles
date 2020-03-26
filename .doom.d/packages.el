d6af0e86c2f6ab7644733780fc1e94649c095680
Mattias Lundberg <me@mlundberg.se>
2020-03-26 16:29:46 +0100

    Install flycheck-mypy

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -25,6 +25,7 @@
 (package! company-tabnine)
 (package! emoji-cheat-sheet-plus)
 (package! dash-at-point)
+(package! flycheck-mypy)
 
 ;; To install a package directly from a particular repo, you'll need to specify
 ;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
