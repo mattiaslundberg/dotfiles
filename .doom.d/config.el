4537ea35f8f694e0192d5a430af9c15328c6e760
Mattias Lundberg <me@mlundberg.se>
2020-05-26 18:42:01 +0200

    Print times nicer and inside prompt

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -90,29 +90,30 @@
 (defun eshell-current-command-start ()
   (setq eshell-current-command-start-time (current-time)))
 
-(defun eshell-current-command-stop ()
-  (when eshell-current-command-start-time
-    (let ((took (float-time
-                 (time-subtract (current-time)
-                                eshell-current-command-start-time))))
-      (if (> took 3)
-          (eshell-interactive-print
-           (format-seconds "%H %M %z%.2ss" took))))
-    (setq eshell-current-command-start-time nil)))
+(defun command-time ()
+  (if eshell-current-command-start-time
+      (let ((took (float-time
+                   (time-subtract (current-time)
+                                  eshell-current-command-start-time))))
+        (setq eshell-current-command-start-time nil)
+        (if (> took 3)
+            (format-seconds "%H %M %z%.2ss " took)
+          ""))
+    ""))
 
 (defun eshell-current-command-time-track ()
-  (add-hook 'eshell-pre-command-hook #'eshell-current-command-start nil t)
-  (add-hook 'eshell-post-command-hook #'eshell-current-command-stop nil t))
+  (add-hook 'eshell-pre-command-hook #'eshell-current-command-start nil t))
 
 (add-hook 'eshell-mode-hook #'eshell-current-command-time-track)
 
 (defun eshell-prompt-fn ()
   (concat (if (bobp) "" "\n")
+          (propertize (command-time) 'face '((t)))
           (propertize (number-to-string eshell-last-command-status)
-                      'face '+eshell-prompt-git-branch)
+                      'face '((t)))
           " "
           (propertize (format-time-string "%H:%M:%S" (current-time))
-                      'face '+eshell-prompt-git-branch)
+                      'face '((t)))
           (+eshell-default-prompt-fn)))
 
 (after! eshell
