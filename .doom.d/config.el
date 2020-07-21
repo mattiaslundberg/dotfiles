122568bc2d5b24c835e870caa87adf0feca80568
Mattias Lundberg <me@mlundberg.se>
2020-07-21 14:06:53 +0200

    Add command to rerun failed pytest tests

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -192,7 +192,8 @@
 (map! :after python
       :localleader
       :map python-mode-map
-      "t a" #'python-pytest)
+      "t a" #'python-pytest
+      "t x" #'python-pytest-last-failed)
 
 (set-popup-rule! "^\\*pytest" :size 0.3 :ttl 0)
 
