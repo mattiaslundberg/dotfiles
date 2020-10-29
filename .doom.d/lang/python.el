551da9665ad23f57ff62229a9d7915585c85ec43
Mattias Lundberg <me@mlundberg.se>
2020-10-29 07:53:12 +0100

    Remove custom keymap
    It's added upstream

--- .doom.d/lang/python.el
+++ .doom.d/lang/python.el
@@ -1,6 +1,5 @@
 (map! :after python
       :localleader
       :map python-mode-map
-      "t a" #'python-pytest
       "t x" #'python-pytest-last-failed)
 (set-popup-rule! "^\\*pytest" :size 0.3 :ttl 0)
