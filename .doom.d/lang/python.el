7cd927823b7a6e037dbeb5f51555da74d92c4aa9
Mattias Lundberg <me@mlundberg.se>
2020-11-03 15:59:44 +0100

    Add tramp pyright config

--- .doom.d/lang/python.el
+++ .doom.d/lang/python.el
@@ -3,3 +3,10 @@
       :map python-mode-map
       "t x" #'python-pytest-last-failed)
 (set-popup-rule! "^\\*pytest" :size 0.3 :ttl 0)
+
+(after! lsp-mode
+  (lsp-register-client
+    (make-lsp-client :new-connection (lsp-tramp-connection "pyls")
+                     :major-modes '(python-mode)
+                     :remote? t
+                     :server-id 'pyls-remote)))
