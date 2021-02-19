60d469a7f15a619213230a65452b9cf5fcf2a886
Mattias Lundberg <me@mlundberg.se>
2021-02-19 08:38:48 +0100

    Cleanup custom formatter
    Locating directory fixed upstream

--- .doom.d/modules/custom/formatting/config.el
+++ .doom.d/modules/custom/formatting/config.el
@@ -6,12 +6,8 @@
   (add-to-list 'apheleia-formatters '(mixformat . ("mix" "format" "-")))
   (add-to-list 'apheleia-mode-alist '(elixir-mode . mixformat)))
 
-(defadvice! custom-apheleia-format-buffer (orig-fn command &optional callback)
+(defadvice! custom-apheleia-format-buffer (&rest _)
   "Run formatter from project root, this is required to get mix format to find config files"
-  :around #'apheleia-format-buffer
+  :before #'apheleia-format-buffer
   ;; Hide lsp ui so it doesn't lock up
-  (lsp-ui-sideline--delete-ov)
-  (if (string-equal "mix" (car command))
-    (let ((default-directory (locate-dominating-file ".formatter.exs" default-directory)))
-      (funcall orig-fn command callback))
-    (funcall orig-fn command callback)))
+  (lsp-ui-sideline--delete-ov))
