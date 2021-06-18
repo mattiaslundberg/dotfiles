c83abe8d1b9b081e2e6c438ed521f7a117f71753
Mattias Lundberg <me@mlundberg.se>
2021-06-18 09:03:47 +0200

    Don't set obsolete alias

--- .doom.d/lang/kotlin.el
+++ .doom.d/lang/kotlin.el
@@ -1,1 +1,1 @@
-(setq lsp-kotlin-language-server-path "~/.kotlin-language-server/server/build/install/server/bin/kotlin-language-server")
+(setq lsp-clients-kotlin-server-executable "~/.kotlin-language-server/server/build/install/server/bin/kotlin-language-server")
