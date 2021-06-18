013b7c6dc4a8e6da69be80648234e3aad6a6820f
Mattias Lundberg <me@mlundberg.se>
2021-06-18 10:15:21 +0200

    Run kotlin tests from emacs

--- .doom.d/lang/kotlin.el
+++ .doom.d/lang/kotlin.el
@@ -1,1 +1,14 @@
 (setq lsp-clients-kotlin-server-executable "~/.kotlin-language-server/server/build/install/server/bin/kotlin-language-server")
+
+
+(defun ml/kotlin-test ()
+  (interactive)
+  (let ((default-directory (+kotlin-locate-gradlew-file))
+        (compilation-read-command nil)
+        (compile-command "sh gradlew clean test"))
+    (call-interactively #'compile)))
+
+(map! :after kotlin-mode
+      :localleader
+      :map kotlin-mode-map
+      (:desc "Run all tests" "t a" #'ml/kotlin-test))
