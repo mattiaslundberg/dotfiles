bb68af06c0942e9e0974dc070bebab2754d8649b
Mattias Lundberg <me@mlundberg.se>
2021-08-24 13:38:17 +0200

    Remove tramp lsp config

--- .doom.d/lang/python.el
+++ .doom.d/lang/python.el
@@ -5,13 +5,6 @@
       "t s" #'python-pytest-file)
 (set-popup-rule! "^\\*pytest" :size 0.3 :ttl 0)
 
-(after! lsp-mode
-  (lsp-register-client
-    (make-lsp-client :new-connection (lsp-tramp-connection "pyls")
-                     :major-modes '(python-mode)
-                     :remote? t
-                     :server-id 'pyls-remote)))
-
 (defadvice! ml/pytest-root ()
   :override #'python-pytest--project-root
   (locate-dominating-file default-directory "pytest.ini"))
