6354d7b099f363ddd48d3e06965391c69464440f
Mattias Lundberg <me@mlundberg.se>
2021-11-15 14:31:49 +0100

    Add notetaking helper

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -18,6 +18,15 @@
 (add-to-list 'default-frame-alist (cons 'width 120))
 (add-to-list 'default-frame-alist (cons 'height 50))
 
+;; Notetaking
+(defun ml/add-note ()
+  (interactive)
+  (let* ((content (read-string "Input note: ")))
+    (f-append-text (format "- %s" content) 'utf-8 "~/notes/todo.md")))
+
+(map! :leader
+  (:desc "Add note" "n n" #'ml/add-note))
+
 ;; Compilation
 (setq compilation-scroll-output t)
 
