ba17329460855e41392d0ba789281713d6ca1a2a
Mattias Lundberg <me@mlundberg.se>
2022-03-24 11:14:10 +0100

    Add macro for extracting python string

--- .doom.d/lang/python.el
+++ .doom.d/lang/python.el
@@ -1,10 +1,15 @@
-(map! :after python
-      :localleader
-      :map python-mode-map
-      "t x" #'python-pytest-last-failed
-      "t s" #'python-pytest-file)
 (set-popup-rule! "^\\*pytest" :size 0.3 :ttl 0)
 
 (defadvice! ml/pytest-root ()
   :override #'python-pytest--project-root
   (locate-dominating-file default-directory "pytest.ini"))
+
+(fset 'extract-python-string
+   (kmacro-lambda-form [?v ?a ?\" ?S ?f ?_ return escape] 0 "%d"))
+
+(map! :after python
+      :localleader
+      :map python-mode-map
+      "t x" #'python-pytest-last-failed
+      "t s" #'python-pytest-file
+      (:desc "Extract string for translation" "e" #'extract-python-string))
