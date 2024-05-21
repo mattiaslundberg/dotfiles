77a2ae71a0dfa4793220131f448bc0b891987dfa
Mattias Lundberg <me@mlundberg.se>
2024-05-21 10:04:28 +0200

    Set dev container from env variable to allow for different node versions

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -38,7 +38,7 @@
 (setq eglot-ignored-server-capabilities '(:documentHighlightProvider))
 (setq eglot-withhold-process-id 1)
 ;; (set-eglot-client! 'rjsx-mode `("typescript-language-server" "--stdio"))
-(set-eglot-client! 'rjsx-mode `("~/.bin/lsp-docker.sh" "ml/devserver-nodejs:18" "typescript-language-server --stdio"))
+(set-eglot-client! 'rjsx-mode `("~/.bin/lsp-docker.sh" "typescript-language-server --stdio"))
 
 ;; Modeline
 (setq doom-modeline-buffer-file-name-style 'truncate-with-project
--- bin/lsp-docker.sh
+++ bin/lsp-docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-exec docker run --rm --interactive --volume /Users/mattias/Development/:/Users/mattias/Development $@
+exec docker run --rm --interactive --volume /Users/mattias/Development/:/Users/mattias/Development ${ML_LSP_DOCKER_CONTAINER:-"ml/devserver-nodejs:18"} $@
